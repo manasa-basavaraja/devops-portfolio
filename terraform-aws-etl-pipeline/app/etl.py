6d356ed88af4c3f0a7d446275633d002a01c07b7
manasa-basavaraja <manasamb13@gmail.com>
2026-04-05 22:45:18 -0400

    aws terraform etl pipeline intial commit

---
+++ terraform-aws-etl-pipeline/app/etl.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import boto3
+from datetime import datetime
+
+def run_etl():
+    # Sample data (replace with API later if needed)
+    data = {
+        "name": ["Manasa", "DevOps"],
+        "role": ["Engineer", "Pipeline"]
+    }
+
+    df = pd.DataFrame(data)
+
+    file_name = f"output_{datetime.now().strftime('%Y%m%d%H%M%S')}.csv"
+
+    s3 = boto3.client('s3')
+    s3.put_object(
+        Bucket="devops-etl-demo-bucket",
+        Key=f"data/{file_name}",
+        Body=df.to_csv(index=False)
+    )
+
+    print("ETL job completed and uploaded to S3")
+
+if __name__ == "__main__":
+    run_etl()
