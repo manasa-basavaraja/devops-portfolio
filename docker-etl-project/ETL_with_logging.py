3dd0a3e0c0d8ea5b468acd7766fd3b1567f28fc4
manasa-basavaraja <manasamb13@gmail.com>
2026-04-08 21:13:38 -0400

    made changes to python script

--- docker-etl-project/ETL_with_logging.py
+++ docker-etl-project/ETL_with_logging.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import logging
 import os
-from datetime import datetime
+# from datetime import datetime
 
 LOG_FILE = f"etl_log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
 
